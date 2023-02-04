a90138935025e4ddaceb87c6c95ad36a5171c1f3
Cao Ha <122292320+chicaoha@users.noreply.github.com>
2023-02-04 10:12:58 +0700

    DataStorage change

---
+++ src/com/clp/chat/Database.java
@@ -0,0 +1,24 @@
+package com.clp.chat;
+
+public class Database {
+	private static Database instance;
+
+	public Database(Database instance) {
+		super();
+		this.instance = instance;
+	}
+
+	public static Database getInstance() {
+		if( instance == null) {
+			synchronized (Database.class) {
+				if(instance == null) {
+					instance = new Database(instance);
+				}
+			}
+		}
+		return instance;
+	}
+
+
+
+}
