0a31bdd858f63e07d1346a8491f93e5e9f39dbe4
Cao Ha <122292320+chicaoha@users.noreply.github.com>
2023-02-18 11:02:20 +0700

    Squashed commit of the following:
    commit 61ab87cbbdd410f820ec8aa3a4806c0825db24c9
    Author: glinh0303 <linh.khong.set17@eiu.edu.vn>
    Date:   Sat Feb 18 10:58:50 2023 +0700
    
        change DataStorage and UserServive
    
    commit ece87ee99639a11f6cbec17ea2ca08621f41f7c8
    Author: glinh0303 <linh.khong.set17@eiu.edu.vn>
    Date:   Sat Feb 18 10:41:41 2023 +0700
    
        commit merge
    
    commit 194f59afcc7885f135a707ec6b2102edf32ea0f1
    Merge: d50b3e9 c3aff5d
    Author: glinh0303 <linh.khong.set17@eiu.edu.vn>
    Date:   Sat Feb 18 10:40:33 2023 +0700
    
        commit merge
    
    commit d50b3e9d39e0b292192042b88252e4e2bfa521d8
    Merge: f1869b7 03b2f25
    Author: glinh0303 <linh.khong.set17@eiu.edu.vn>
    Date:   Sat Feb 18 10:39:01 2023 +0700
    
        Merge branch 'main' into Linh

--- src/com/clp/data/DataStorage.java
+++ src/com/clp/data/DataStorage.java
@@ -1,14 +1,13 @@
 package com.clp.data;
 
+
 import com.clp.entity.User;
 import com.clp.repository.Repository;
 
 public class DataStorage {
 	protected Repository<User> users;
 
-	public void setUsers(Repository<User> users) {
-		this.users = users;
+	public Repository<User> getUsers() {
+		return users;
 	}
-	
-	
 }
