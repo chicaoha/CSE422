61ab87cbbdd410f820ec8aa3a4806c0825db24c9
glinh0303 <linh.khong.set17@eiu.edu.vn>
2023-02-18 10:58:50 +0700

    change DataStorage and UserServive

--- src/com/clp/data/DataStorage.java
+++ src/com/clp/data/DataStorage.java
@@ -7,7 +7,7 @@ import com.clp.repository.Repository;
 public class DataStorage {
 	protected Repository<User> users;
 
-	public void setUsers(Repository<User> users) {
-		this.users = users;
+	public Repository<User> getUsers() {
+		return users;
 	}
 }
