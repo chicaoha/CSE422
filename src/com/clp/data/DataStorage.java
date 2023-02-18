20ff189bd66b7a73d32d9b6d1399dd0ee8907bc6
Cao Ha <122292320+chicaoha@users.noreply.github.com>
2023-02-18 15:10:12 +0700

    commit unit test

--- src/com/clp/data/DataStorage.java
+++ src/com/clp/data/DataStorage.java
@@ -1,10 +1,8 @@
 package com.clp.data;
-
-
 import com.clp.entity.User;
 import com.clp.repository.Repository;
 
-public class DataStorage {
+public abstract class DataStorage implements Repository<User>  {
 	protected Repository<User> users;
 
 	public Repository<User> getUsers() {
