ac95923758fcf8df8fd20b986129818c252e7066
HoangPhat1910 <118871312+HoangPhat1910@users.noreply.github.com>
2023-03-04 08:54:29 +0700

    Update BaseEntity.java

--- src/com/clp/entity/BaseEntity.java
+++ src/com/clp/entity/BaseEntity.java
@@ -1,7 +1,7 @@
 
 package com.clp.entity;
 
-public class BaseEntity {
+public abstract class BaseEntity {
 	private int id;
 
 	public int getId() {
@@ -10,5 +10,11 @@ public class BaseEntity {
 
 	public void setId(int id) {
 		this.id = id;
+	}
+
+	public void setIdIfNotExist(int id) {
+		if (id != 0) {
+			setId(id);
+		}
 	}
 }
