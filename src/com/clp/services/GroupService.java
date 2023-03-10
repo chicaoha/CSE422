32cae60e6eb60f1c023cd1467a6b21b7470a99d4
Cao Ha <cao_ha@datahouse.com>
2023-03-10 23:11:15 +0700

    Squashed commit of the following:
    commit d268a7456da87f3c4bbdadd43b2af43df33875e1
    Author: glinh0303 <linh.khong.set17@eiu.edu.vn>
    Date:   Tue Mar 7 11:15:46 2023 +0700
    
        create group

--- src/com/clp/services/GroupService.java
+++ src/com/clp/services/GroupService.java
@@ -1,29 +1,53 @@
 package com.clp.services;
-
-import java.util.ArrayList;
 import java.util.List;
 
-import com.clp.chat.PublicGroup;
+import com.clp.data.DataStorage;
 import com.clp.entity.Group;
 import com.clp.entity.PrivateGroup;
+import com.clp.entity.PublicGroup;
 import com.clp.entity.User;
+import com.clp.repository.Repository;
 
 public class GroupService {
-	List<Group> listGroups = new ArrayList<Group>();
-	
-	public List<Group> createGroup(User user, PublicGroup publicGroup, PrivateGroup privateGroup, String groupType) {
-		int userId = user.getId();
-		if (groupType.equalsIgnoreCase("public")) {
-			listGroups.add(publicGroup);
-		} else {
-			listGroups.add(privateGroup);
-		}
-		return listGroups;
+//	private List<Group> groups;
+	private final DataStorage storage;
+	private boolean isPublic;
+	private String groupName;
+	private List<PublicGroup> listPublicGroup;
+	private List<PrivateGroup> listPrivateGroup;
+
+	public GroupService(DataStorage storage, boolean isPublic, String groupName) {
+		super();
+		this.storage = storage;
+		this.isPublic = isPublic;
+		this.groupName = groupName;
 	}
-	public void inviteUser(String user) {
-		if(listGroups.contains(user)) {
-			System.out.println(user + " is already a member of this group.");
+
+	public Group createGroup(User user, boolean isPublic) {
+		Repository<Group> groupRepository = storage.getGroup();
+		Group group;
+		if (isPublic) {
+			PublicGroup publicGroup = new PublicGroup();
+			String code = publicGroup.getGroupCode();
+			listPublicGroup.add(publicGroup);
+			group = publicGroup;
+		} else {
+			PrivateGroup privateGroup = new PrivateGroup(user);
+			group = privateGroup;
 		}
+		storage.getGroup().insert(group);
+		return group;
+	}
+
+	public boolean RemoveUserFromGroup(int userId) {
+		boolean flag = false;
+		User foundUser = getUserById(userId);
+		if (foundUser != null) {
+			users.remove(foundUser);
+			flag = true;
+		}
+		return flag;
+
 		
 	}
 }
